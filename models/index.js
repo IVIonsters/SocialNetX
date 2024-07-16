d26c4cd7396e73fded039f9b67a0aba863ca1f62
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-16 18:28:23 -0500

    Update index.js
    Exporting the User and Thought models to be used in the server.js file.

---
+++ models/index.js
@@ -0,0 +1,5 @@
+//Exporting the User and Thought models to be used in the server.js file.
+const User = require('./User');
+const Thought = require('./Thought');
+
+module.exports = { User, Thought };
