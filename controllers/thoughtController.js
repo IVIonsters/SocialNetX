48bc25639052077dd44efecc5f067056c742e1de
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-07-17 11:32:17 -0500

    Update thoughtController.js
    created functions for creation of thought and to update thought, added error checking and response if no creation or update

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -24,13 +24,41 @@ module.exports = {
         } catch (error) {
             res.status(500).json({ message: 'Something went wrong!, no thought found', details: error });
         }
-    }
+    },
 
     // create thought
-
+    async cThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            // find the user that created the thought
+            await User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $push: { thoughts: thought._id } },
+                { new: true }
+            );
+            res.json(thought);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Something went wrong!, thought not created', details: error });
+        }
+    },
 
     // update thought
-
+    async uThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true } // new: true returns the updated thought
+            );
+            // if no thought is found
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong!, thought not updated', details: error });
+        }
+    },
 
     // delete thought
 
