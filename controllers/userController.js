53480508390e186ebde731eca11561fec3380104
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-07-17 14:49:54 -0500

    Update userController.js
    added functions to create user, update user with error logging and responses to track issue's.
    
    added delete user , add friend and delete friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,16 +31,88 @@ module.exports = {
     },
 
     // create user
-
+    async cUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong!, user not created', details: error });
+        }
+    },
 
     // update user
-
+    async uUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            // if no user is found
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            res.json(user);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Something went wrong!, user not updated', details: error });
+        }
+    },
 
     // delete user
+    async dUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
 
+            // if no user is found
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            // delete all thoughts associated with the user
+            await Thought.deleteMany({ username: user.username });
+            res.json({ message: 'User thoughts deleted!' });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Something went wrong!, user not deleted', details: error });
+        }
+    },
 
     // add friend
+    async aFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { new: true }
+            ).populate('friends');
 
+            // if no user is found
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Something went wrong!, friend not added', details: error });
+        }
+    },
 
     // delete friend
+    async dFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: req.params.friendId } },
+                { new: true }
+            ).populate('friends');
+
+            // if no user is found
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            res.json(user);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Something went wrong!, friend not deleted', details: error });
+        }
+    },
 };
