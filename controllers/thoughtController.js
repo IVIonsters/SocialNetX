85c9f50d60de80b9cadc060ae1d85a5ab2d0b817
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-16 19:14:23 -0500

    Controller Setup
    Imported models for thoughtController and userController.
    
    Implemented pseudo code for functions to create in each controller.
    Get all
    Get Single
    Create
    Updated
    Delete
    Add
    Delete

---
+++ controllers/thoughtController.js
@@ -0,0 +1,25 @@
+// Importing the Thought and User models
+const Thought = require('../models/Thought');
+const User = require('../models/User');
+
+module.exports = {
+    // get all thoughts
+
+
+    // get one thought
+
+
+    // create thought
+
+
+    // update thought
+
+
+    // delete thought
+
+
+    // add reaction
+
+
+    // delete reaction
+}
