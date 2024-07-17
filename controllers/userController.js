30f6d4f367d73c9d11ad1fad03193eba9646fe74
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-07-17 13:32:36 -0500

    Update userController.js
    added functions to get all and get one user, added error control and messages to track possible issues

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,10 +3,32 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     // get all users
-
+    async gUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json({ message: 'Something went wrong!, users not retrieved', details: err });
+        }
+    },
 
     // get one user
-
+    async gOneUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v')
+                .populate('thoughts')
+                .populate('friends');
+    
+            // if no user is found
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            res.json(user);
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong!, no user found', details: error });
+        }
+    },
 
     // create user
 
