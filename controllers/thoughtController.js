077b89c0904309d268b83ed5283bf7aa3f83c88c
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-07-17 12:20:17 -0500

    Update thoughtController.js
    created functions to delete thought, add and delete reactions. Added error catching and messages to track potential issues

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -61,10 +61,53 @@ module.exports = {
     },
 
     // delete thought
+    async dThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+            // if no thought is found
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
 
+            res.json({ message: 'Thought deleted!' });
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong!, thought not deleted', details: error });
+        }
+    }, 
 
     // add reaction
+    async aReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $push: { reactions: req.body } },
+                { new: true, runValidators: true }
+            );
 
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json(thought);
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong!, reaction not added', details: error });
+        }
+    },
 
     // delete reaction
+    async dReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { reactions: { reactionId: req.params.reactionId } } },
+                { new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json({ message: 'Reaction deleted!' });
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong!, reaction not deleted', details: error });
+        }
+    }
 }
