cb2e7de7fcd74eb6e81782898a404c71592f4937
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-16 20:01:11 -0500

    Update thoughtController.js
    setup functions to retrieve all and single thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,10 +4,27 @@ const User = require('../models/User');
 
 module.exports = {
     // get all thoughts
-
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json({ message: 'Something went wrong! , thoughts not retrieved', details: err });
+        }
+    },
 
     // get one thought
-
+    async getOneThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+            // if no thought is found
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong!, no thought found', details: error });
+        }
+    }
 
     // create thought
 
